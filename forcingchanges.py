27ba8365e03cfa393f1656ee91391e1c796f727c
Derek McMahon <dmm7aj@virginia.edu>
2016-02-22 17:28:32 -0500

    Useless changes

---
+++ forcingchanges.py
@@ -0,0 +1,6 @@
+__author__ = "dmm7aj"
+
+if __name__ == "__main__":
+    x =5
+    z = 7
+    print(str(x+z))
--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ import helper
 
 if __name__== "__main__":
     helper.greeting("hello")
+    helper.greeting(7)
